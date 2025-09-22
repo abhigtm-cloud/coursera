e4cc2dff862c4d708826cf483a21ea76e4198937
abhi <abhigtm3003@gmail.com>
2025-09-22 21:40:08 +0530

    coursera course progress

---
+++ resources/views/user.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <h1>User Details</h1>
+    @if($user)
+        <p><strong>Name:</strong> {{ $user->name }}</p>
+        <p><strong>Email:</strong> {{ $user->email }}</p>
+        <p><strong>Joined:</strong> {{ $user->created_at->format('M d, Y') }}</p>
+    @else
+        <p>No user found.</p>
+    @endif
+</body>
+</html>
